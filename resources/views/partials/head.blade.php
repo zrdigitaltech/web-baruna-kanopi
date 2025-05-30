0241d0f75cd040f2c208e1fa5440681398cf203e
zikriramdani <zikriramdani@gmail.com>
2025-05-30 23:07:22 +0700

    Progress: UI

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,5 +1,5 @@
 <!-- SEO by ZRDigitalTech -->
-<title>Jasa Pemasangan Kanopi Profesional & Terpercaya | Baruna Kanopi</title>
+<title>Jasa Pemasangan Kanopi | Baruna Kanopi</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="description" content="Penyedia jasa pemasangan kanopi terpercaya untuk rumah, toko, dan bangunan komersial di Indonesia.">
